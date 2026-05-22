661296ef4e5471d2213aa33e64e0095b1daf02b7
Md Fazlul Karim Sium <sium.2305095@bau.edu.bd>
2026-05-22 16:08:27 +0600

    Add login form submission handling
    Implement login functionality with validation for admin credentials.

---
+++ auth.js
@@ -0,0 +1,24 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(
+username === "admin" &&
+password === "admin123"
+){
+window.location.href =
+"dashboard.html";
+}
+else{
+alert("Invalid Credentials");
+}
+
+});
